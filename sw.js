dd8282bd77c5f41637c9d9904f062b1d7eea8355
viDaLost <vitaledanilov@gmail.com>
2026-09-20 18:11:33 +0300

    Cache recovered actor and boss sprites

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='mega-worm-ru-v5';
-const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon.svg','./assets/original-assets.js','./assets/original-levels.js','./assets/original/GroundStrip_Tex.png','./assets/original/Background_Classic_Tex.png','./assets/original/Wojira_Tex.png'];
+const CACHE='mega-worm-ru-v6';
+const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon.svg','./assets/original-assets.js','./assets/original-levels.js','./assets/original/GroundStrip_Tex.png','./assets/original/Background_Classic_Tex.png','./assets/original/Wojira_Tex.png','./assets/original/Actors_Web_Tex.png','./assets/original/Actor_Bosses_Tex.png','./assets/original/Actor_Bosses1_Tex.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;}).catch(()=>caches.match('./index.html'))));});
