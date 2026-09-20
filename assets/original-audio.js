f12e27388014f54337ab24e700c6caf51a13594a
viDaLost <vitaledanilov@gmail.com>
2026-09-20 18:34:25 +0300

    Add recovered original audio mapping

---
+++ assets/original-audio.js
@@ -0,0 +1,7 @@
+// Original AudioClip files recovered from the supplied Super Mega Worm 2.0.0 APK.
+window.RECOVERED_AUDIO = Object.freeze({
+  pickup_emp: './assets/audio/pickup_emp.mp3',
+  worm_fire_spit: './assets/audio/worm_fire_spit.mp3',
+  cow_death: './assets/audio/cow_death.mp3',
+  wmd_level_up: './assets/audio/wmd_level_up.mp3'
+});
